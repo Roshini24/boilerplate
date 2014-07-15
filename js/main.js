02fe0ddad5a0021816e65a4a6ead0678d54ea756
Roshini24 <rpp44@njit.edu>
2014-07-14 21:17:31 -0400

    main
    main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function checkempty(){
 
+if(document.getElementById("txtname").value ==""){
+	alert("name is empty");}
+else 	{
+
+if (document.getElementById("txtname").value.length < 4 ){
+	alert("Name length cannot be less than 4");
+}
+alert("name is not empty");
+}
+	
+}
